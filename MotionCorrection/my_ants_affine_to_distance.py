d3bc858f98a95b185bbd530e1c7ffb08b35e0405
27-apizzuti <a.pizzuti@maastrichtuniversity.nl>
2022-11-08 16:26:25 +0100

    Fix typo in line 29

--- MotionCorrection/my_ants_affine_to_distance.py
+++ MotionCorrection/my_ants_affine_to_distance.py
@@ -15,18 +15,18 @@ import numpy as np
 def my_ants_affine_to_distance(affine, unit):
 
     dx, dy, dz = affine[9:]
-    
+
     #according to https://itk.org/Doxygen/html/classitk_1_1AffineTransform.html
     #the first 9 elementes are stored in row major order. This is the same
     #order used in C/C++ and Python (while R and MATLAB follow colmajor order)
-    
-    #See "Rigid Body Registration" (John Ashburner & Karl J. Friston) to have 
+
+    #See "Rigid Body Registration" (John Ashburner & Karl J. Friston) to have
     #an explanation of how to select the parameters in the rotation matrix
-    
+
     rot_y = np.arcsin(affine[2])
     cos_rot_y = np.cos(rot_y)
     rot_x = np.arctan2(affine[5] / cos_rot_y, affine[8] / cos_rot_y)
-    rot_z = np.arctan2(affine[2] / cos_rot_y, affine[0] / cos_rot_y)
+    rot_z = np.arctan2(affine[1] / cos_rot_y, affine[0] / cos_rot_y)
 
     if unit == 'deg':
         deg = np.degrees
